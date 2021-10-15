cb6a8ea79cc68bc039a1db803d1ce570ff86c0d6
Thatcher Deyoe <tdeyoe2@gmail.com>
2021-10-15 00:59:49 -0400

    idk how the hell to work with vue

---
+++ client/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <h1>For The Love Of Fitness </h1>
+    </header>
+</template>
+
+
+
+<script>
+export default {
+    name: 'Header',
+
+}
+</script>
+
+<style scoped>
+    header{
+    background-image: url('chalkboard-blackboard-dark-wallpaper-background_28629-1125.jpg');
+    height: 150px;
+    margin-top: 0%;
+    
+}
+</style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+        <div class="login">
+        <h1>Login</h1>
+        <form method="post" action="" name="login">
+            <input type="text" name="username" pattern="[a-zA-Z0-9]+" placeholder="login" id="username" required />
+            <input type="password" name="password" placeholder="password" required>
+            <input type="submit" name="login" value="Login">
+        </form>
+    </div>
+
+</template>
+
+<style>
+    
+</style>
+
+<script>
+    export default {
+        data: ()=>({
+        username: null,
+        password: null,
+        Session
+    })
+}
+</script>
