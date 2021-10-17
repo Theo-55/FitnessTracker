2b70a2003fc732647e463107092f2dab72870ad9
Thatcher Deyoe <tdeyoe2@gmail.com>
2021-10-17 13:56:42 -0400

    midterm vue routing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,20 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
-
 const routes = [
     {
       path: '/',
-      name: 'Login',
-      component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
+      name: 'Home',
+      component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue'),
   
-    },
-  
+    }
   ]
   
   const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
   })
-  
   export default router
