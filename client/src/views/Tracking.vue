ee38646c3922b50a3d43572e8a8b3c87af6c88bc
Thatcher Deyoe <tdeyoe2@gmail.com>
2021-11-18 20:39:29 -0500

    feed for posting workouts

--- client/src/views/Tracking.vue
+++ client/src/views/Tracking.vue
@@ -1,55 +1,43 @@
 <template>
-    <div class="container">
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-              <a class="navbar-item" >
-                <img src="../css/barbell.jpg" width="112" height="28">
-              </a>
-          
-              <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </a>
-            </div>
-          
-            <div id="navbarBasicExample" class="navbar-menu">
-              <div class="navbar-start">
-                <a class="navbar-item" id="create" onclick="testFunction()">
-                  Create
-                </a>
-          
-                <a class="navbar-item">
-                  Read
-                </a>
 
-                <a class="navbar-item">
-                    Update
-                </a>
-                
-                <a class="navbar-item">
-                    Delete
-                </a>
+            <div class="card" style="background-color: #777b7ec2;">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content" style="background-color: #777b7ec2;">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">John Smith</p>
+        <p class="subtitle is-6">@johnsmith</p>
+      </div>
+    </div>
 
-                <a class="navbar-item" href="Home.vue">
-                  Home
-              </a>
-                </div>
-          </div>
-        </nav>
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br>
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+    </div>
+  </div>
 </div>
+
+
+
+
 </template>
 
+<script>
 
+</script>
 
 <style src="../css/tracking.css">
 
 </style>
-        
-
-<script>
-
-
-  
-</script>
-
