05eb5e3b098b779318a2313aaa22622df21ebe2e
Thatcher Deyoe <tdeyoe2@gmail.com>
2021-11-20 17:32:20 -0500

    server not connecting?

--- server/models/mongo.js
+++ server/models/mongo.js
@@ -2,7 +2,7 @@
 */
 
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.memar.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.memar.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
